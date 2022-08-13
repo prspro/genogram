d83f59afb7e549210aa683e1fffc3d060e1abeec
prospero <prospero1135@gmail.com>
2022-08-14 02:39:21 +0300

    prepared project commit

---
+++ src/components/Person/Person.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import usePerson from './usePrson';
+import "./person.sass";
+
+const Person = () => {
+  return (
+    <div>Person</div>
+  )
+};
+
+export default Person;
---
+++ src/components/Person/usePrson.ts
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const usePerson = () => {
+  return {}
+}
+
+export default usePerson
